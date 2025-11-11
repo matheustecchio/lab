7adbb36755afec13c3b1d0bee6eda4e5cea873a1
Matheus Tecchio <matheusdiastecchio@gmail.com>
2025-11-11 23:07:58 +0000

    Setup Go

---
+++ projects/restAPI/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	server := gin.Default()
+
+	server.GET("/events", getEvents)
+
+	server.Run(":8080")
+
+}
+
+func getEvents(context *gin.Context) {
+	context.JSON(http.StatusOK, gin.H{"message": "Hello"})
+}
